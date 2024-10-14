1624fd0a5addabcbdd264a59588e9cb2fd117ef2
braianricci <braianricci@gmail.com>
2024-10-14 09:05:24 -0300

    carousel and start of animation

--- tpe2/js/script.js
+++ tpe2/js/script.js
@@ -25,12 +25,20 @@ async function partialRender(event) {
     main.innerHTML = content;
     loader.classList.remove("show-loader");
 
+
     listenersLinks();
+
+    switch (this.getAttribute("href")) {
+        case "home.html":
+            startCarousel(1);
+            break;
+    }
 }
 
 function listenersLinks() {
     let links = document.getElementsByClassName("link");
-    let cardToggles = document.getElementsByClassName("card-toggle")
+    let cardToggles = document.getElementsByClassName("card-toggle");
+    let bannerInputs = document.getElementsByClassName("carousel-radio");
 
     for (let item of links) {
         item.addEventListener("click", partialRender);
@@ -38,6 +46,10 @@ function listenersLinks() {
 
     for (let item of cardToggles) {
         item.addEventListener("click", updateCard);
+    }
+
+    for (let item of bannerInputs) {
+        item.addEventListener("change", moveCarousel)
     }
 }
 
@@ -101,4 +113,34 @@ function animateLoader(duration) {
     setTimeout(() => {
         bar.classList.remove("loader-bar-animation");
     }, duration + 600);
+}
+
+async function startCarousel(position) {
+
+    const inputs = document.getElementById("carousel-inputs");
+    const input = inputs.querySelector(`input[value="${position}"]`);
+
+    if (input) {
+
+        const event = new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        });
+
+        input.dispatchEvent(event);
+    }
+
+    position = (position % 4) + 1;
+    setTimeout(() => startCarousel(position), 5000);
+}
+
+function moveCarousel(event) {
+
+    const carousel = document.getElementById("carousel");
+    const value = event.target.value;
+    const item = document.getElementById("carousel-" + value);
+    item.classList.add("carousel-animation");
+
+    carousel.style.setProperty("--position", value);
 }
