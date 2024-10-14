345d68302ebde2c35c24a3319dbe6c82dbfe16be
braianricci <braianricci@gmail.com>
2024-10-14 15:06:09 -0300

    sidebar start

--- tpe2/js/script.js
+++ tpe2/js/script.js
@@ -26,42 +26,42 @@ async function PartialRender(event) {
 
     ListenerCards();
 
-  //agregamos EventListeners a los nuevos elementos con las siguientes funciones
-  //ListenersLinks();
+    //agregamos EventListeners a los nuevos elementos con las siguientes funciones
+    //ListenersLinks();
 
-  //this.href en chrome devuelve una url completa, no la direccion relativa, por eso usamos this.getAttribute("href") para comparar
-  /*switch (this.getAttribute("href")) {
-    case "productos.html":
-      if (this.id == "index") scrollTop = 0;
-      break;
-    case "mochilas.html":
-    case "mates.html":
-    case "gorras.html":
-    case "rinioneras.html":
-    case "cintos.html":
-    case "bolsos.html":
-      ListenersProducts();
-      break;
-    case "contacto.html":
-      ListenersCaptcha();
-      break;
-    case "carrito.html":
-      ListenersCart();
-  }*/
+    //this.href en chrome devuelve una url completa, no la direccion relativa, por eso usamos this.getAttribute("href") para comparar
+    /*switch (this.getAttribute("href")) {
+      case "productos.html":
+        if (this.id == "index") scrollTop = 0;
+        break;
+      case "mochilas.html":
+      case "mates.html":
+      case "gorras.html":
+      case "rinioneras.html":
+      case "cintos.html":
+      case "bolsos.html":
+        ListenersProducts();
+        break;
+      case "contacto.html":
+        ListenersCaptcha();
+        break;
+      case "carrito.html":
+        ListenersCart();
+    }*/
 }
 
 function ListenersLinks() {
-  let links = document.getElementsByClassName("link");
+    let links = document.getElementsByClassName("link");
 
-  for (let item of links) {
-    item.addEventListener("click", PartialRender);
-  }
+    for (let item of links) {
+        item.addEventListener("click", PartialRender);
+    }
 }
 
 function ListenerCards() {
     let cards = document.getElementsByClassName("card");
 
-    for(let card of cards) {
+    for (let card of cards) {
         let blackout = card.children[1];
 
         card.addEventListener("mouseenter", () => {
@@ -74,4 +74,26 @@ function ListenerCards() {
             blackout.classList.remove('card-blackout-visible');
         })
     }
+}
+
+let btnOpen = document.getElementById("btn-open");
+let btnClose = document.getElementById("btn-close");
+let btnProd = document.getElementById("btn-prod");
+
+btnOpen.addEventListener("click", OpenSidebar);
+btnClose.addEventListener("click", CloseSidebar);
+btnProd.addEventListener("click", ToggleProd);
+
+function OpenSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+function CloseSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
+
+function ToggleProd() {
+    document
+        .getElementById("sidebar-sub")
+        .classList.toggle("sidebar-sub-display");
 }
