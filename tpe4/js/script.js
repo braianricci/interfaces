47336a75e33a080615a74c8463f4c06a9d0e9b2a
Esteban19899 <evillanueva@alumnos.exa.unicen.edu.ar>
2024-11-15 08:20:37 -0300

    animacion Section 3

--- tpe4/js/script.js
+++ tpe4/js/script.js
@@ -27,3 +27,20 @@ function updateOffset(positions, debug) {
 
     debug.innerHTML = '&emsp;Current scroll y value: ' + debugInfo;
 }
+
+/* ╔══════━━━━━━────── • 3 - descarga • ──────━━━━━━══════╗ */
+
+const sectionDescarga = document.querySelector('.section.descarga');
+const imagen = document.querySelector('.boys');
+
+if (sectionDescarga && imagen) {
+    sectionDescarga.addEventListener('mousemove', (event) => {
+        const { left, top, width, height } = sectionDescarga.getBoundingClientRect();
+        const offsetX = ((event.clientX - left) / width) * 2 - 1;
+        const offsetY = ((event.clientY - top) / height) * 2 - 1;
+
+        // Ajustar la cantidad de movimiento
+        const moveAmount = 30;
+        imagen.style.transform = `translate(${-offsetX * moveAmount}px, ${-offsetY * moveAmount}px) scale(1.1)`;
+    });
+}
